dfaed8ebc438e113ab0b4c90eab712dd3e85a8f3
Christian <138394851+zeitferne@users.noreply.github.com>
2023-07-17 21:05:28 +0200

    Add notebooks

--- src/export.py
+++ src/export.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+import re
 from subprocess import check_call
 from shutil import which, rmtree
 import os
@@ -9,40 +11,55 @@ from argparse import ArgumentParser
 def main():
     parser = ArgumentParser()
     parser.add_argument("--sync-to-s3")
+    parser.add_argument("--execute", action="store_true")
     args = parser.parse_args()
 
-    output_dir = "export"
+    pdir = Path(__file__).absolute().parent.parent
+    assert (pdir / ".git").is_dir(), "Expected git dir at " + str(pdir)
+    pdir = re.escape(str(pdir.parent).replace("\\", "/"))
+    if len(pdir) > 2 and pdir[1] == ":":
+        pdir = f"(?:{pdir})|(?:{pdir[2:]})"
+    pdir = f"(?:{pdir})|(?:{re.escape(os.path.realpath(Path.home()))})"
+    pdir = f"(?:{pdir})|(?:{re.escape(str(Path.home()))})"
+    pdir = pdir.replace("/", r"[/\\]")
+    pdir_pat = re.compile(pdir.encode("utf-8"), re.IGNORECASE)
+
+
+
+    output_dir = "gh-pages"
     if os.path.exists(output_dir):
         rmtree(output_dir)
     os.mkdir(output_dir)
-    exportargs =  [
-        which("jupyter"),
-        "nbconvert",
-        "--to=html",
-        "--no-input",
-        "--ExtractOutputPreprocessor.enabled=true",
-        "--output-dir=" + output_dir,
-        "covidat.ipynb",
-    ]
-    if not args.only_export:
-        exportargs.append("--execute")
-    check_call(exportargs)
-    fname = os.path.join(output_dir, "covidat.html")
-    with open(fname, "rb") as htmlfile:
-        html = htmlfile.read()
-    html = html.replace(
-        b"<title>",
-        b"""
+
+    for nbfile in Path(".").glob("*.ipynb"):
+        exportargs =  [
+            which("jupyter"),
+            "nbconvert",
+            "--to=html",
+            #"--no-input",
+            "--ExtractOutputPreprocessor.enabled=true",
+            "--output-dir=" + output_dir,
+            str(nbfile),
+        ]
+        if args.execute:
+            exportargs.append("--execute")
+        check_call(exportargs)
+        fname = output_dir / nbfile.with_suffix(".html")
+        with open(fname, "rb") as htmlfile:
+            html = htmlfile.read()
+        html = html.replace(
+            b"<title>",
+            b"""
 <meta name="robots" content="noindex">
 <style>
-        html, body { max-width: 800px; margin: auto; }
+        html, body { max-width: 1000px; margin: auto; }
 </style>
 <title>""",
-        1,
-    )
-    with open(os.path.join(output_dir, "index.html"), "wb") as htmlfile:
-        htmlfile.write(html)
-    os.unlink(fname)
+            1,
+        )
+        html = pdir_pat.sub(b"...", html)
+        with open(fname, "wb") as htmlfile:
+            htmlfile.write(html)
 
     if args.sync_to_s3:
         subprocess.check_call(["aws", "s3", "sync", output_dir, "s3://" + args.sync_to_s3, "--delete"])

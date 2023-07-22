c09fbd8d53a7e66fd5606d67852014426aae674e
Christian <138394851+zeitferne@users.noreply.github.com>
2023-07-22 16:14:54 +0200

    collectshortage: Add load_veasp_xml

--- src/collectshortage.py
+++ src/collectshortage.py
@@ -85,7 +85,9 @@ def load_azr():
         "[-,][^/]+$", "", regex=True, n=1
     )
     azr["nkey"] = norm_name(azr["Name"])
-    azr_m = pd.read_csv(util.DATAROOT / "basg-medicineshortage/ASP-Missing.csv", sep=";")
+    azr_m = pd.read_csv(
+        util.DATAROOT / "basg-medicineshortage/ASP-Missing.csv", sep=";"
+    )
     azr = pd.concat([azr, azr0, azr_m]).reset_index(drop=True)
     return azr
 
@@ -108,13 +110,71 @@ cat_prio = [
     "verfügbar",
 ][::-1]
 
-def processfile(dts: Set[date], fname: Path, azr: pd.DataFrame, writer: csv.DictWriter):
-    fdate = (
-        datetime.strptime(
-            fname.stem.split("_", 1)[1].split(".", 1)[0], "%Y%m%d_%H%M%S"
-        )
-        .date()
+
+def load_veasp_xml(fname, azr: pd.DataFrame, only_statagg=False) -> pd.DataFrame:
+    veasp = pd.read_xml(fname, parser="etree", xpath="./Packungen/Packung")
+    veasp.rename(
+        columns={"Bezeichnung_Arzneispezialitaet": "Name"},
+        inplace=True,
+        errors="raise",
     )
+    veasp["Zulassungsnummer"] = veasp["Zulassungsnummer"].str.strip()
+    veasp = veasp.merge(
+        azr[["Zulassungsnummer", "Verwendung"]],
+        "left",
+        on="Zulassungsnummer",
+    )
+    msk = pd.isna(veasp["Verwendung"]) & veasp["Zulassungsnummer"].str.match(
+        "EU/.+[-,]"
+    )
+    veasp.loc[msk, "Zulassungsnummer"] = veasp.loc[msk, "Zulassungsnummer"].str.replace(
+        "[-,][^/]+$", "", regex=True, n=1
+    )
+    veasp = veasp.merge(
+        azr[["Zulassungsnummer", "Verwendung"]],
+        "left",
+        on="Zulassungsnummer",
+        suffixes=("", "_y"),
+    )
+    # display(veasp)
+    veasp["Status"].replace("Nicht verfügbar", "nicht verfügbar", inplace=True)
+    veasp["Verwendung"] = veasp["Verwendung"].combine_first(veasp["Verwendung_y"])
+    naveasp = veasp[pd.isna(veasp["Verwendung"])]
+    if len(naveasp) > 0:
+        raise ValueError(
+            str(fname)
+            + ": Not found in AZR: "
+            + naveasp[["Zulassungsnummer", "Name"]].to_csv(sep=";", index=False)
+        )
+    # veasp.sort_values(["Avail_c"], kind="stable", inplace=True)
+    # veasp.drop(columns=["Verwendung_y"], inplace=True)
+    try:
+        agg = {
+            "Name": "first",
+            "Status": agg_status,
+        }
+        if not only_statagg:
+            agg |= {
+                "Grund": lambda g: " / ".join(g.unique()),
+                "Melder": lambda w: " / ".join(w.unique()),
+                "Melder": lambda w: " / ".join(w.unique()),
+                "Zulassungsinhaber": lambda w: " / ".join(w.unique()),
+                "Wirkstoffe": lambda w: pd.unique(", ".join(w.unique()).split(", ")),
+                "Datum_Meldung":lambda s: pd.to_datetime(s, format="%Y-%m-%d").min(),
+                "Datum_letzte_Aenderung": lambda s: pd.to_datetime(s, format="%Y-%m-%d").max(),
+                "Beginn_Vertriebseinschraenkung": lambda s: pd.to_datetime(s, format="%Y-%m-%d").min()
+            }
+        return (
+            veasp.groupby(["Zulassungsnummer", "Verwendung"]).agg(agg)
+        ).reset_index()
+    except StopIteration:
+        raise ValueError(f"{fname}: Bad status: {veasp['Status'].unique()}")
+
+
+def processfile(dts: Set[date], fname: Path, azr: pd.DataFrame, writer: csv.DictWriter):
+    fdate = datetime.strptime(
+        fname.stem.split("_", 1)[1].split(".", 1)[0], "%Y%m%d_%H%M%S"
+    ).date()
     if fdate in dts:
         return
     if fname.name.endswith(".xlsx"):
@@ -129,7 +189,7 @@ def processfile(dts: Set[date], fname: Path, azr: pd.DataFrame, writer: csv.Dict
         )
         for c, dt in data.dtypes.items():
             if dt == "object":
-                #print("s", c, dt)
+                # print("s", c, dt)
                 data[c] = data[c].str.strip()
         data["Status"] = data["Status"].str.rstrip("*")
         # data = data.query("Status != 'verfügbar'")
@@ -141,70 +201,21 @@ def processfile(dts: Set[date], fname: Path, azr: pd.DataFrame, writer: csv.Dict
             "Status",
         ] = "teilweise verfügbar"
         data.loc[
-            (~data["PZN eingeschränkt verfügbarer Packungen"].astype(bool)
-            | pd.isna(data["PZN eingeschränkt verfügbarer Packungen"]))
+            (
+                ~data["PZN eingeschränkt verfügbarer Packungen"].astype(bool)
+                | pd.isna(data["PZN eingeschränkt verfügbarer Packungen"])
+            )
             & (data["Status"] == "eingeschränkt verfügbar"),
             "Status",
         ] = "teilweise verfügbar"
     else:
         source_format = "xml"
-        veasp = pd.read_xml(fname, parser="etree", xpath="./Packungen/Packung")
-        veasp.rename(
-            columns={"Bezeichnung_Arzneispezialitaet": "Name"},
-            inplace=True,
-            errors="raise",
-        )
-        veasp["Zulassungsnummer"] = veasp["Zulassungsnummer"].str.strip()
-        veasp = veasp.merge(
-            azr[["Zulassungsnummer", "Verwendung"]],
-            "left",
-            on="Zulassungsnummer",
-        )
-        msk = pd.isna(veasp["Verwendung"]) & veasp[
-            "Zulassungsnummer"
-        ].str.match("EU/.+[-,]")
-        veasp.loc[msk, "Zulassungsnummer"] = veasp.loc[
-            msk, "Zulassungsnummer"
-        ].str.replace("[-,][^/]+$", "", regex=True, n=1)
-        veasp = veasp.merge(
-            azr[["Zulassungsnummer", "Verwendung"]],
-            "left",
-            on="Zulassungsnummer",
-            suffixes=("", "_y"),
-        )
-        # display(veasp)
-        veasp["Status"].replace(
-            "Nicht verfügbar", "nicht verfügbar", inplace=True
-        )
-        veasp["Verwendung"] = veasp["Verwendung"].combine_first(
-            veasp["Verwendung_y"]
-        )
-        naveasp = veasp[pd.isna(veasp["Verwendung"])]
-        if len(naveasp) > 0:
-            raise ValueError(
-                str(fname)
-                + ": Not found in AZR: "
-                + naveasp[["Zulassungsnummer", "Name"]].to_csv(
-                    sep=";", index=False
-                )
-            )
-        # veasp.sort_values(["Avail_c"], kind="stable", inplace=True)
-        # veasp.drop(columns=["Verwendung_y"], inplace=True)
-        try:
-            data = (
-                veasp[["Verwendung", "Status", "Zulassungsnummer", "Name"]]
-                .groupby(["Zulassungsnummer", "Verwendung"])
-                .agg({"Name": "first", "Status": agg_status})
-            ).reset_index()
-        except StopIteration:
-            raise ValueError(f"{fname}: Bad status: {veasp['Status'].unique()}")
+        data = load_veasp_xml(fname, azr, only_statagg=True)
 
     if False:  # Group by name
         dkey = norm_name(data["Name"])
         try:
-            data = data.groupby([dkey, data["Verwendung"]]).agg(
-                {"Status": agg_status}
-            )
+            data = data.groupby([dkey, data["Verwendung"]]).agg({"Status": agg_status})
         except StopIteration:
             raise ValueError(f"{fname}: Bad status: {data['Status'].unique()}")
         # print(fname)
@@ -212,7 +223,7 @@ def processfile(dts: Set[date], fname: Path, azr: pd.DataFrame, writer: csv.Dict
         # input("...")
     for usage, data_u in data.groupby("Verwendung"):
         for avail, data_uv in data_u.groupby("Status"):
-            #if avail == 'verfügbar':
+            # if avail == 'verfügbar':
             #    print(fname)
             #    print(data_uv)
             #    input("...")
@@ -222,7 +233,7 @@ def processfile(dts: Set[date], fname: Path, azr: pd.DataFrame, writer: csv.Dict
                     "Usage": usage,
                     "Availability": avail,
                     "N": len(data_uv),
-                    "SourceFormat": source_format
+                    "SourceFormat": source_format,
                 }
             )
     dts.add(fdate)
@@ -234,13 +245,7 @@ def collectshortage_ex(dirname, outname):
     with open(outname, "w", encoding="utf-8", newline="\n") as outfile:
         writer = csv.DictWriter(
             outfile,
-            (
-                "Datum",
-                "Usage",
-                "Availability",
-                "N",
-                "SourceFormat"
-            ),
+            ("Datum", "Usage", "Availability", "N", "SourceFormat"),
             delimiter=";",
         )
         writer.writeheader()
@@ -255,11 +260,16 @@ def collectshortage_ex(dirname, outname):
                 processfile(dts, fname, azr, writer)
             except Exception as e:
                 traceback.print_exc()
-                print("The previous exception occured during processing of", fname, file=sys.stderr, flush=True)
+                print(
+                    "The previous exception occured during processing of",
+                    fname,
+                    file=sys.stderr,
+                    flush=True,
+                )
 
 
 def main():
-    collectdir =  util.COLLECTROOT / "medshort"
+    collectdir = util.COLLECTROOT / "medshort"
     collectdir.mkdir(parents=True, exist_ok=True)
     datadir = util.DATAROOT / "basg-medicineshortage"
 

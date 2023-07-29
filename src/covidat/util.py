19d1f10c9473541505b64f26990acd76cb8f3e55
Christian <138394851+zeitferne@users.noreply.github.com>
2023-07-29 20:25:26 +0200

    WIP: Workflow to update notebook

--- src/covidat/util.py
+++ src/covidat/util.py
@@ -9,4 +9,4 @@ DL_TSTAMP_FMT = "%Y%m%d_%H%M%S"
 DATAROOT = Path(os.getenv("COVAT_DATA_ROOT", "../covidat-data/data"))
 COLLECTROOT = Path(os.getenv("COVAT_COLLECT_ROOT", "tmpdata"))
 TZ_AT = ZoneInfo("Europe/Vienna")
-LOG_FORMAT = "%(asctime)s:%(levelname)s:%(name)s:%(message)s"
+LOG_FORMAT = "%(asctime)s:%(levelname)s:%(name)s: %(message)s"

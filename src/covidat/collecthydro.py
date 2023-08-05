df045708c91e795b4d380f7b8f6753def25153b9
Christian <138394851+zeitferne@users.noreply.github.com>
2023-08-05 23:17:05 +0200

    abwassermonitoring: Add plot for R_eff

--- src/covidat/collecthydro.py
+++ src/covidat/collecthydro.py
@@ -12,30 +12,34 @@ from . import util
 logger = logging.getLogger(__name__)
 
 
-def collecthydro(prefixname: str, *, insubdir: bool):
+def collecthydro(prefixname: str, *, has_uww: bool, insubdir: bool, namecol: str = "name"):
     subdir = "covid/abwassermonitoring"
     outname = util.COLLECTROOT / subdir / (prefixname + "_all.csv.xz")
     outname.parent.mkdir(parents=True, exist_ok=True)
-    with lzma.open(outname, "wt", encoding="utf-8", newline="\n", preset=1) as outfile:
+    with lzma.open(outname, "wt", encoding="utf-8", newline="", preset=1) as outfile:
         writer = csv.writer(outfile, delimiter=";")
-        writer.writerow(["FileDate", "Datum", "name", "uwwcode", "y"])
+        writer.writerow(["FileDate", "Datum", namecol, *(("uwwcode",) if has_uww else ()), "y"])
         n = 0
         for fname in sorted(
             (util.DATAROOT / subdir).glob("????/" + (f"/{prefixname}/" if insubdir else "") + prefixname + "_*_*.json")
         ):
             # print(fname)
             with open(fname, "rb") as f:
-                fdata = json.load(f)
+                try:
+                    fdata = json.load(f)
+                except json.JSONDecodeError as e:
+                    logger.warn("Failed parsing JSON in %s: %s", fname, e)
+                    continue
             fdate = (
                 datetime.strptime(fname.stem.removeprefix(prefixname + "_"), util.DL_TSTAMP_FMT)
                 .replace(tzinfo=UTC)
                 .isoformat()
             )
             for linedata in fdata["data"]:
-                name = linedata["name"]
-                uww = linedata["uwwcode"]
+                name = linedata[namecol]
+                uww = (linedata["uwwcode"],) if has_uww else ()
                 for x, y in zip_longest(linedata["x"], linedata["y"]):
-                    writer.writerow([fdate, x, name, uww, y])
+                    writer.writerow([fdate, x, name, *uww, y])
             n += 1
         logger.info("%s: Collected %d files", prefixname, n)
 
@@ -46,8 +50,9 @@ def main():
         format=util.LOG_FORMAT,
     )
 
-    collecthydro("blverlauf", insubdir=True)
-    collecthydro("natmon_01", insubdir=False)
+    collecthydro("natmon_02", has_uww=False, insubdir=False, namecol="typ")
+    collecthydro("blverlauf", has_uww=True, insubdir=True)
+    collecthydro("natmon_01", has_uww=True, insubdir=False)
 
 
 if __name__ == "__main__":
